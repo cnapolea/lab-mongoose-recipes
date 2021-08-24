4459d66d36cc3b63080a35e20fadacdca0ad0264
cnapolea <cjnapoleao.dev@gmail.com>
2021-08-24 21:24:04 +0100

    Complete lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,54 @@
+// jshint esversion:10
+
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+const schemaStructure = {
+  
+  title: {
+    type: 'String',
+    required: true,
+    unique: true
+  },
 
+  level: {
+    type: 'String',
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+
+  ingredients: [String],
+  
+  cuisine: {
+    type: String,
+    required: true
+  },
+
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg',
+  },
+
+  duration: {
+    type: Number,
+    min: 0
+  },
+
+  creator: String,
+
+  created: {
+    type: Date,
+    default: Date.now(),
+  }
+
+};
+
+const recipeSchema = new mongoose.Schema(schemaStructure);
 const Recipe = mongoose.model('Recipe', recipeSchema);
+
+
 
 module.exports = Recipe;
